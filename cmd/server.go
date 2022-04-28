45ae49099b187ea56f8fdf1f90877e5d96e83800
Robin <robin1998@hotmail.no>
2022-04-28 18:39:50 +0200

    removed print statement

--- cmd/server.go
+++ cmd/server.go
@@ -3,13 +3,10 @@ package main
 import (
 	constants "assignment_2"
 	"assignment_2/handlers"
-	"fmt"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("Running")
-
 	http.HandleFunc(constants.DEFAULT_PATH, handlers.DefaultHandler)
 	http.HandleFunc(constants.CASES_PATH, handlers.CasesHandler)
 	http.HandleFunc(constants.POLICY_PATH, handlers.PolicyHandler)
