d8cc04eb960ac71a85a4e7d30c031a9ca4cf5250
Robin <robin1998@hotmail.no>
2022-04-28 18:48:24 +0200

    removed blanked lines

--- handlers/handlerCases.go
+++ handlers/handlerCases.go
@@ -23,13 +23,10 @@ Function to give the user a response from a GET request*/
 func casesGetRequest(w http.ResponseWriter, r *http.Request) {
 	search := path.Base(r.URL.Path)
 	if search != "cases" {
-
 		w.Header().Set("content-type", "application/json")
-
 		http.Error(w, string(readjson.ReadCasesApi(search)), http.StatusOK)
 		WebhookCall(w, r, search)
-
 	} else {
-		http.Error(w, "You may have tried a different http request than GET or you have not entered a search word", http.StatusOK)
+		http.Error(w, "You may have tried a different http request than GET or you have not entered a search word", http.StatusMethodNotAllowed)
 	}
 }
--- handlers/handlerDefault.go
+++ handlers/handlerDefault.go
@@ -11,6 +11,6 @@ func DefaultHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	default:
 		http.Error(w, "To use the api use the following URL's \n"+constants.CASES_PATH+"\n"+
-			constants.POLICY_PATH+"\n"+constants.NOTIFICATION_PATH+"\n"+constants.STATUS_PATH, http.StatusAccepted)
+			constants.POLICY_PATH+"\n"+constants.NOTIFICATION_PATH+"\n"+constants.STATUS_PATH, http.StatusOK)
 	}
 }
